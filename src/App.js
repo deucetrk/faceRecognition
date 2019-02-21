22cbf5847ecc33e8b602a189561dac4a9116a200
Teerakarn <sl16394@hotmail.com>
2019-02-21 11:06:32 +0700

    done changes

--- src/App.js
+++ src/App.js
@@ -41,12 +41,14 @@ class App extends Component {
     this.setState({imageUrl: this.state.input});
 
 
-    app.models.predict(
-      Clarifai.COLOR_MODEL, 
-      this.state.input).then(
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL, 
+      this.state.input)
+      .then(
     function(response) {
       // do something with response
-      console.log(response)
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
     },
     function(err) {
       // there was an error
